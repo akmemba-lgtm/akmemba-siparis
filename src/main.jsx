ba8a31810281b6b2b1da49f3e43c0fe8f3a019d2
Akmemba <akmemba@gmail.com>
2026-05-16 14:41:52 +0300

    driver route

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
-import DriverPanel from "./DriverPanel.jsx";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-const path = window.location.pathname;
+import App from "./App";
+import DriverPanel from "./DriverPanel";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import "./index.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    {path === "/driver" ? (
-      <DriverPanel />
-    ) : (
-      <App />
-    )}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+
+        <Route
+          path="/driver"
+          element={<DriverPanel />}
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
